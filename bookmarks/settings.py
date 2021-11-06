8d9137bac44652910dd5b46816b59cbbb9ddda73
melvin-shaju <melvinshaju155@gmail.com>
2021-11-06 12:25:15 +0530

    Bookmarking feature in progress. Not complete. Need to add ajax related code.

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2g!@mf9!ahu02z_29_@u%efw5cmst55%1_&--&y20x!2n!u&r3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
+    'images.apps.ImagesConfig',
 ]
 
 MIDDLEWARE = [
